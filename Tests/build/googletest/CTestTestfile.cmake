5904a2755cbb2c34197586e2aa7d4d27a1763002
Kristina <christina@softhenge.com>
2025-01-24 15:02:12 +0400

    Removed build from remote repo

--- Tests/build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kristina/hash_table/googletest
-# Build directory: /home/kristina/hash_table/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- Tests/build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kristina/hash_table/googletest/googletest
-# Build directory: /home/kristina/hash_table/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
